31a96ab0931193db80d2305584bfafa9456ad5ce
Haresh Suresh <haresh.suresh@sysdig.com>
2024-08-12 02:17:28 -0700

    add single & org examples

---
+++ test/examples/organization/onboarding_with_cspm.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "sysdig" {
+  sysdig_secure_url       = "https://secure-staging.sysdig.com"
+  sysdig_secure_api_token = "<API_TOKEN>"
+}
+
+terraform {
+  required_providers {
+    sysdig = {
+      source  = "sysdiglabs/sysdig"
+      version = "~> 1.28"
+    }
+  }
+}
+
+module "onboarding" {
+  source            	  = "../../../modules/onboarding"
+  trusted_identity  	  = "arn:aws:iam::064689838359:role/us-east-1-integration01-secure-assume-role"
+  external_id       	  = "81145517f4fafde4ade30b01762b7b0b"
+  organizational_unit_ids = []
+  is_organizational 	  = true
+}
+
+module "config-posture" {
+  source            = "../../../modules/services/config-posture"
+  trusted_identity  = "arn:aws:iam::064689838359:role/us-east-1-integration01-secure-assume-role"
+  external_id       = "81145517f4fafde4ade30b01762b7b0b"
+  role_name         = "sysdig-secure-2u6g"
+  org_units         = []
+  is_organizational = true
+}
+
+resource "sysdig_secure_cloud_auth_account_feature" "config_posture" {
+  account_id = module.onboarding.sysdig_secure_account_id
+  type       = "FEATURE_SECURE_CONFIG_POSTURE"
+  enabled    = true
+  components = [module.config-posture.config_posture_component_id]
+  depends_on = [module.config-posture]
+}
