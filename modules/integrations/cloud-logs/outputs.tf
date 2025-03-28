51d2de156b628c352bd01e16c1fcfb57dbadcd9c
lorenzo merici <lorenzo.merici@sysdig.com>
2025-03-28 16:11:08 +0100

    fix output

--- modules/integrations/cloud-logs/outputs.tf
+++ modules/integrations/cloud-logs/outputs.tf
@@ -1,5 +1,5 @@
 output "cloud_logs_component_id" {
-  value       = sysdig_secure_cloud_auth_account_component.aws_cloud_logs.id
+  value       = "${sysdig_secure_cloud_auth_account_component.aws_cloud_logs.type}/${sysdig_secure_cloud_auth_account_component.aws_cloud_logs.instance}"
   description = "Component identifier of Cloud Logs integration created in Sysdig Backend for Log Ingestion"
   depends_on  = [sysdig_secure_cloud_auth_account_component.aws_cloud_logs]
 }
