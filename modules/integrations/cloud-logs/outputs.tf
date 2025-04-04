81c0b1d79d32a66f9a6f2c5a4d896909b492e7bd
lorenzo-merici <110896035+lorenzo-merici@users.noreply.github.com>
2025-04-04 18:06:51 +0200

    feat(cloudlogs): cross account bucket support (#43)

--- modules/integrations/cloud-logs/outputs.tf
+++ modules/integrations/cloud-logs/outputs.tf
@@ -1,5 +1,15 @@
 output "cloud_logs_component_id" {
   value       = "${sysdig_secure_cloud_auth_account_component.aws_cloud_logs.type}/${sysdig_secure_cloud_auth_account_component.aws_cloud_logs.instance}"
   description = "Component identifier of Cloud Logs integration created in Sysdig Backend for Log Ingestion"
-  depends_on = [ sysdig_secure_cloud_auth_account_component.aws_cloud_logs ]
+  depends_on  = [sysdig_secure_cloud_auth_account_component.aws_cloud_logs]
+}
+
+output "kms_policy_instructions" {
+  description = "Instructions for updating KMS key policy when KMS encryption is enabled"
+  value = (var.kms_key_arn != null) ? templatefile(
+    "${path.module}/templates/kms_policy_instructions.tpl",
+    {
+      role_arn = "arn:${data.aws_partition.current.partition}:iam::${local.bucket_account_id}:role/${local.role_name}"
+    }
+  ) : ""
 }
