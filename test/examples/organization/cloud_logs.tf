8aaf8ab64719aeae9bbb6bcf43c35cb62ff45b7c
Ajay Rangarajan <109552447+ajay-rangarajan@users.noreply.github.com>
2025-04-14 15:59:27 -0500

    SSPROD-54180 logless ciem test updates (#57)
    * SSPROD-54180 aws logless ciem test updates
    
    * SSPROD-54180 adding ignore changes to basic feature
    
    * SSPROD-54180 ignore basic ciem flag and component changes

--- test/examples/organization/cloud_logs.tf
+++ test/examples/organization/cloud_logs.tf
@@ -17,10 +17,15 @@ resource "sysdig_secure_cloud_auth_account_feature" "threat_detection" {
   depends_on = [module.cloud-logs]
 }
 
-resource "sysdig_secure_cloud_auth_account_feature" "identity_entitlement" {
+resource "sysdig_secure_cloud_auth_account_feature" "identity_entitlement_advanced" {
   account_id = module.onboarding.sysdig_secure_account_id
   type       = "FEATURE_SECURE_IDENTITY_ENTITLEMENT"
   enabled    = true
-  components = [module.cloud-logs.cloud_logs_component_id]
-  depends_on = [module.cloud-logs, sysdig_secure_cloud_auth_account_feature.config_posture]
+  components = concat(sysdig_secure_cloud_auth_account_feature.identity_entitlement_basic.components, [module.cloud-logs.cloud_logs_component_id])
+  depends_on = [module.cloud-logs, sysdig_secure_cloud_auth_account_feature.identity_entitlement_basic]
+  flags = {"CIEM_FEATURE_MODE": "advanced"}
+
+  lifecycle {
+    ignore_changes = [flags, components]
+  }
 }
