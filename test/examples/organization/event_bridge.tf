12008a00954b45e81d73deca3941745f83f259ce
Mital Shah <mital.shah@sysdig.com>
2025-11-05 12:30:48 -0800

    internal: remove basic ciem from depends on to match snippet with UI

--- test/examples/organization/event_bridge.tf
+++ test/examples/organization/event_bridge.tf
@@ -31,7 +31,7 @@ resource "sysdig_secure_cloud_auth_account_feature" "identity_entitlement_advanc
   type       = "FEATURE_SECURE_IDENTITY_ENTITLEMENT"
   enabled    = true
   components = concat(sysdig_secure_cloud_auth_account_feature.identity_entitlement_basic.components, [module.event-bridge.event_bridge_component_id])
-  depends_on = [module.event-bridge, sysdig_secure_cloud_auth_account_feature.identity_entitlement_basic]
+  depends_on = [module.event-bridge]
   flags      = { "CIEM_FEATURE_MODE" : "advanced" }
 
   lifecycle {
