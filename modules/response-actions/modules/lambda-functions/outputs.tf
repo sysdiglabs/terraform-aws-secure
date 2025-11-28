c6cc6335dee6c9fe01ed9f645433aeecf48b847b
Mirko Bonasorte <mirko.bonasorte@sysdig.com>
2025-11-28 16:36:21 +0100

    Removing unnecessary stuff

--- modules/response-actions/modules/lambda-functions/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "function_arn" {
-  description = "ARN of the Lambda function"
-  value       = aws_lambda_function.function.arn
-}
-
-output "function_name" {
-  description = "Name of the Lambda function"
-  value       = aws_lambda_function.function.function_name
-}
