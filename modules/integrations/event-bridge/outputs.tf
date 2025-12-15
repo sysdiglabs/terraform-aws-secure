ea80fbc2482a7414305f6f772dcda57bd6d113dd
Ivan Besinovic <ivan.besinovic@sysdig.com>
2025-12-15 11:38:07 +0100

    SSPROD-57377 - Move output to outputs.tf

--- modules/integrations/event-bridge/outputs.tf
+++ modules/integrations/event-bridge/outputs.tf
@@ -3,3 +3,8 @@ output "event_bridge_component_id" {
   description = "Component identifier of Event Bridge integration created in Sysdig Backend for Log Ingestion"
   depends_on  = [sysdig_secure_cloud_auth_account_component.aws_event_bridge]
 }
+
+output "post_ciem_basic_delay" {
+  value       = var.wait_after_basic_seconds > 0 ? time_sleep.wait_after_ciem_basic : null
+  description = "Wait handle to delay downstream operations after basic (e.g., CIEM) by the configured seconds."
+}
