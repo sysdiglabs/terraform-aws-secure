c5f2a498da84f6953ad477acd220623487ad4a9e
Mirko Bonasorte <mirko.bonasorte@sysdig.com>
2026-01-15 17:05:06 +0100

    Locally testing

--- modules/response-actions/versions.tf
+++ modules/response-actions/versions.tf
@@ -10,13 +10,8 @@ terraform {
       version = ">= 3.1"
     }
     sysdig = {
-      source  = "local/sysdiglabs/sysdig"
-      version = "1.0.0"
+      source  = "sysdiglabs/sysdig"
     }
-#     sysdig = {
-#       source  = "sysdiglabs/sysdig"
-#       version = "~> 3.3"
-#     }
     time = {
       source  = "hashicorp/time"
       version = ">= 0.9"
